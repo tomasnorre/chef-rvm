9742f33f64ca941dc30cf7df8d2779a75eeb1db5
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2015-03-05 21:23:48 +0100

    Add fetch key signature before installing

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,6 +11,7 @@ bash 'install_rvm' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
+  curl -sSL https://rvm.io/mpapis.asc | gpg --import -
   curl -sSL https://get.rvm.io | bash -s stable --ruby
   source /usr/local/rvm/scripts/rvm
   EOH
